2fac57b26a2bf9ed6679f4a6657bdbe8dca6c479
Igor Olszewski <71021482+IgorOlszewski2000@users.noreply.github.com>
2021-12-21 14:13:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var tekst = "Witaj";
+
+let liczba = 4;
+
+const imie = "Igor";
+
+liczba = 7;
+
+console.log(tekst + " " + liczba + " " + imie);
+
+// Pobieranie wartości od użytkownika
+
+const nazwa = prompt("Podaj imie: ");
+
