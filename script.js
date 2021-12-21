695f29882cb6b2a7c1e9f37839a4b8d3737c3415
Igor Olszewski <71021482+IgorOlszewski2000@users.noreply.github.com>
2021-12-21 14:58:54 +0000

    init2

--- script.js
+++ script.js
@@ -10,10 +10,52 @@ console.log(tekst + " " + liczba + " " + imie);
 
 // Pobieranie wartości od użytkownika
 
-const nazwa = prompt("Podaj imie: ");
-alert("Dzień dobry " + nazwa);
+//const nazwa = prompt("Podaj imie: ");
+//alert("Dzień dobry " + nazwa);
 
-const liczba1 = prompt("Podaj liczbe 1: ");
-const liczba2 = prompt("Podaj liczbe 2: ");
+//const liczba1 = Number(prompt("Podaj liczbe 1: "));
+//const liczba2 = Number(prompt("Podaj liczbe 2: "));
 
-console.log("Suma wynosi: " + liczba1 + liczba2)
+//console.log("Suma wynosi: " + (liczba1 + liczba2));
+
+//let wynik = liczba1 * liczba2;
+
+//wynik++;
+//wynik += 10;
+//console.log("Mnożenie: " + ++wynik);
+
+// zwykła funkcja
+function start(){
+  alert("Hello");
+}
+
+//funkcja strzałkowa
+const startArrow = () => {
+  alert("Hello");
+}
+
+// funkcje ze zwracaniem wartości
+const checkAlko = () =>{
+  const age = Number(prompt("Ile masz lat? "));
+  if(age >= 18)
+  {
+    return "Możesz kupić alkohol";
+  }
+  else
+  {
+    return "Nie możesz kupić alkoholu";
+  }
+}
+
+const sumuj = () => 4+10;
+
+const setDiv = (w, h) => {
+  document.getElementById("mietek").style.height = h;
+  document.getElementById("mietek").style.width = w;
+}
+
+function name()
+{
+  alert("Dzień dobry " + arguments[0] + " "  + arguments[1]);
+}
+
